d4f9f584bd0e91dab4dc3a60a8faf88ed53ba3cd
Aryan271 <27aryanraj@gmail.com>
2021-06-14 19:20:38 +0530

    made register and login page

--- server.js
+++ server.js
@@ -11,29 +11,29 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('public'));
 
 
-app.get('/', (req,res) => {
-    res.render('home');
-})
-
 // set Template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname,'/resources/views'));
 app.set('view engine', 'ejs');
 
 
+// Routes
 
+app.get('/', (req,res) => {
+    res.render('home');
+})
 
+app.get('/cart', (req,res) => {
+    res.render('customers/cart');
+})
 
+app.get('/login', (req,res) => {
+    res.render('auth/login');
+})
 
-
-
-
-
-
-
-
-
-
+app.get('/register',(req,res) => {
+    res.render('auth/register');
+})
 
 app.listen(PORT, () => {
     console.log(`LISTENING ON PORT ${PORT}`);
